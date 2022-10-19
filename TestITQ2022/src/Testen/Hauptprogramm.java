7279bd48ef7fb483e6707c22be13ec6342f1224c
manu <gra.man.lang@gmail.com>
2022-10-19 10:24:49 +0200

    first commit

---
+++ TestITQ2022/src/Testen/Hauptprogramm.java
@@ -0,0 +1,26 @@
+package Testen;
+
+import java.util.Scanner;
+
+public class Hauptprogramm {
+
+	public static void main(String[] args) {
+		
+		/* Variablendeklaration */
+		short breite;
+		short hoehe;
+		short flaeche;
+		
+		/* Insanzbildung */
+		Scanner scan = new Scanner(System.in);
+		
+
+		System.out.println("Flächenberechnung");
+		System.out.println("=================");
+		System.out.println();
+		System.out.println("Breite: ");
+		
+		
+	}
+
+}
