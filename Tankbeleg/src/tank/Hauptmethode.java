a3178197cb29e844050a127e49b0afe0851b9096
manu <gra.man.lang@gmail.com>
2022-11-15 14:10:01 +0100

    Add Plausibilitaetspruefung

--- Tankbeleg/src/tank/Hauptmethode.java
+++ Tankbeleg/src/tank/Hauptmethode.java
@@ -13,31 +13,39 @@ public class Hauptmethode {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		while (run= true) {
+		while (run == true) {
 			System.out.println("1\tTankbeleg eingeben");
 			System.out.println("0\tEnde");
-			
+
 			byte eingabe = scan.nextByte();
 			
 			if(eingabe==1) {
-				System.out.println("Bitte Menge in Liter angeben:\n");
+				// Eingabe
+				System.out.println("Bitte Menge in Liter angeben:");
 				l += scan.nextDouble();
+				
 				System.out.println("Bitte Kilometerzahl eingeben:");
 				kmMenge = scan.nextInt();
 				
+				// Plausibilitaetspruefung
+				while(kmMenge <= kmAnfang) {
+					System.out.println("Eingabe darf nicht kleiner als Anfangsbetrag sein!");
+					System.out.println("Bitte Kilometerzahl eingeben:");
+					kmMenge = scan.nextInt();
+				}
+				
 				System.out.printf("Durchschnitt:\t%.2f\n", ((float)(l/((kmMenge-kmAnfang)/100f))));
+				System.out.println();
 			} else if (eingabe==0) {
 				run=false;
+				System.out.println("[!]Goodbye");
 			} else {
 				System.out.println("Falsche Nummer bro!");
 			}
-
-			
 			
 		}
-		
-		
-		
+
+
 	}
 
 }
